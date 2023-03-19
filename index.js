33badec8939ee9a06535cbefeaa7c5d3e101803d
o13o <hoangtunguyen2002@gmail.com>
2023-03-19 16:08:15 +0700

    add some truthy condition

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 // Lesson 1,2,3,4,5,6,7,8,9,10,
 // 11,12,13,14,15,16,17,18,19,20,
 // 21,22,23,24,25,26,27,28,29,30
-// 31,32,33,34,35,36,37
-
-let myLeads = [];
+// 31,32,33,34,35,36,37,38,39,40
 
 const inputEl = document.querySelector(".input-el");
 const inputBtn = document.querySelector(".input-btn");
 const ulEl = document.querySelector(".ul-el");
 
+let myLeads = [];
+myLeads = JSON.parse(localStorage.getItem("myLeads")); // parse to array
+renderLeads();
+
 inputBtn.addEventListener("click", function () {
-  myLeads = JSON.parse(localStorage.getItem("myLeads")); // parse to array
-  if (inputEl.value != "") myLeads.push(inputEl.value);
-  renderLeads(); // render leads to screen 
-  localStorage.setItem("myLeads", JSON.stringify(myLeads)); // store leads to local storage 
+  if (inputEl.value) { // check if input value is truthy value then push
+    myLeads.push(inputEl.value);
+    renderLeads(); 
+  } 
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)); // store leads to local storage
   inputEl.value = ""; // clear input field after saving lead
 });
 
 function renderLeads() {
+  // render leads to screen
   let listItems = ``;
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `
