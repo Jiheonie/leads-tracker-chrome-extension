f1c60050fdfd5abf2f8a1d0e90b7b8d1b85adfeb
o13o <hoangtunguyen2002@gmail.com>
2023-03-19 14:11:08 +0700

    first commit before deploying

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-//lesson 1:
+// Lesson 1,2,3,4,5,6,7,8,9,10,
+// 11,12,13,14,15,16,17,18,19,20,
+// 21,22,23,24,25,26,27,28,29
 
-//lesson 2: add button and input tag
+let myLeads = [];
+const inputEl = document.querySelector(".input-el");
+const inputBtn = document.querySelector(".input-btn");
+const ulEl = document.querySelector(".ul-el");
+const result = document.querySelector(".result");
 
-//lesson 3: style the button and input tag
+inputBtn.addEventListener("click", function () {
+  if (inputEl.value != "") myLeads.push(inputEl.value);
+  renderLeads();
+  result.innerHTML = "Saved!";
+  inputEl.value = "";
+});
 
-//lesson 4: make the input button work with onclick
+function renderLeads() {
+  let listItems = ``;
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li>
+      <a href="${myLeads[i]}" target="_blank"> 
+        ${myLeads[i]}
+      </a>
+    </li>`; // not single quote
+    //using backtick character
+  }
+  // Instead of using innerHTML 3 times inside for loop
+  // just need to use innerHTML 1 times outside for loop
+  // improving performance
+  ulEl.innerHTML = listItems;
+}
+
+// Adding a button using js
+// const body = document.querySelector("body");
+// body.innerHTML += `<button class="buy-btn">Buy!</button>`;
+
+// const buyBtn = document.querySelector(".buy-btn");
+// buyBtn.addEventListener("click", function () {
+//   body.innerHTML += `<p>Thank you for buying!</p>`;
+// });
