10784a3e401aa9ed5ed8d52c7c88eaa8e2c70db9
o13o <hoangtunguyen2002@gmail.com>
2023-03-19 17:01:40 +0700

    refactor code

--- index.js
+++ index.js
@@ -2,40 +2,21 @@
 // 11,12,13,14,15,16,17,18,19,20,
 // 21,22,23,24,25,26,27,28,29,30
 // 31,32,33,34,35,36,37,38,39,40
+// 41,42,43
 
 const inputEl = document.querySelector(".input-el");
 const inputBtn = document.querySelector(".input-btn");
 const deleteBtn = document.querySelector(".delete-btn");
 const ulEl = document.querySelector(".ul-el");
 
-let myLeads = [];
-myLeads = JSON.parse(localStorage.getItem("myLeads")); // parse to array
-renderLeads();
-
-inputBtn.addEventListener("click", function () {
-  if (inputEl.value) {
-    // check if input value is truthy value then push
-    myLeads.push(inputEl.value);
-    renderLeads();
-  }
-  localStorage.setItem("myLeads", JSON.stringify(myLeads)); // store leads to local storage
-  inputEl.value = ""; // clear input field after saving lead
-});
-
-deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
   // render leads to screen
   let listItems = ``;
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-      <a href="${myLeads[i]}" target="_blank"> 
-        ${myLeads[i]}
+      <a href="${leads[i]}" target="_blank"> 
+        ${leads[i]}
       </a>
     </li>`; // not single quote
     //using backtick character
@@ -45,3 +26,24 @@ function renderLeads() {
   // improving performance
   ulEl.innerHTML = listItems;
 }
+
+let myLeads = [];
+myLeads = JSON.parse(localStorage.getItem("myLeads")); // parse to array
+render(myLeads);
+
+inputBtn.addEventListener("click", function () {
+  if (inputEl.value) {
+    // check if input value is truthy value then push
+    myLeads.push(inputEl.value);
+    render(myLeads);
+  }
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)); // store leads to local storage
+  inputEl.value = ""; // clear input field after saving lead
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
